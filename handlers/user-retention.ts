59be70cc0a6c1cb315c0fce545d252db062d7af7
dj <13580441+gary02@users.noreply.github.com>
2023-01-09 01:59:30 +0000

    refactor(user-retention): minor

--- handlers/user-retention.ts
+++ handlers/user-retention.ts
@@ -7,9 +7,10 @@ import { processUserRetention } from "../lib/user-retention";
 // SENDGRID_API_KEY
 // MATTERS_SITE_DOMAIN
 // MATTERS_NEW_FEATURE_TAG_ID;
+// MATTERS_USER_RETENTION_INTERVAL_IN_DAYS
 
 const intervalInDays =
-  parseInt(process.env.USER_RETENTION_INTERVAL_IN_DAYS as string, 10) || 5;
+  parseInt(process.env.MATTERS_USER_RETENTION_INTERVAL_IN_DAYS as string, 10) || 5;
 
 export const handler = async (event: any) => {
   const limit = event.limit;
