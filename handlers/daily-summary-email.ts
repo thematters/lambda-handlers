0e225713309988e9e3907f797f3a964d87466a9b
dj <13580441+gary02@users.noreply.github.com>
2023-01-31 14:03:54 +0000

    doc: minor

--- handlers/daily-summary-email.ts
+++ handlers/daily-summary-email.ts
@@ -2,6 +2,9 @@ import { sendDailySummaryEmails } from "../lib/daily-summary-email/index.js";
 
 // envs
 // MATTERS_AWS_CLOUD_FRONT_ENDPOINT
+// MATTERS_SENDGRID_API_KEY
+// MATTERS_SITE_DOMAIN
+// MATTERS_PG_*
 
 export const handler = async (event: any) => {
   await sendDailySummaryEmails();
