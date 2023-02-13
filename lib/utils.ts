8ff1c58fc3ab74351a151fd54dca64fef144bd53
dj <13580441+gary02@users.noreply.github.com>
2023-02-13 06:33:30 +0000

    refactor(archive-user): move helpers to utils file

--- lib/utils.ts
+++ lib/utils.ts
@@ -48,6 +48,7 @@ export const getPostgresJsClient = (connectionString: string) =>
   });
 
 // AWS
+
 const s3 = new S3Client({ region: process.env.AWS_REGIONAWS_REGION });
 
 export const s3DeleteFile = async (bucket: string, key: string) =>
