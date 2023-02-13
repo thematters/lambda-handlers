5e97e60e461120a0ade72ff5c589be76f01c9453
dj <13580441+gary02@users.noreply.github.com>
2023-02-13 10:12:29 +0000

    feat(refresh-view): add refresh-view lambda

---
+++ lib/refresh-view.ts
@@ -0,0 +1,26 @@
+import { pgKnex as knex } from "../db.js";
+
+export const refreshView = async (view: string) => {
+  if (!Object.values(MATERIALIZED_VIEW).includes(view)) {
+    throw Error(`Unexpected view name: ${view}`);
+  }
+  await knex.raw(/*sql*/ `
+    create unique index if not exists ${view}_id on public.${view} (id);
+    refresh materialized view concurrently ${view}
+  `);
+};
+
+enum MATERIALIZED_VIEW {
+  tag_count_materialized = "tag_count_materialized",
+  user_reader_materialized = "user_reader_materialized",
+  featured_comment_materialized = "featured_comment_materialized",
+  curation_tag_materialized = "curation_tag_materialized",
+  article_hottest_materialized = "article_hottest_materialized",
+  most_active_author_materialized = "most_active_author_materialized",
+  most_appreciated_author_materialized = "most_appreciated_author_materialized",
+  most_trendy_author_materialized = "most_trendy_author_materialized",
+  user_activity_materialized = "user_activity_materialized",
+  recently_read_tags_materialized = "recently_read_tags_materialized",
+  article_read_time_materialized = "article_read_time_materialized",
+  recommended_articles_from_read_tags_materialized = "recommended_articles_from_read_tags_materialized",
+}
