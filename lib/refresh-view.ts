5bc953c439c7d4d347dbc5ba8df5577e788add80
dj <13580441+gary02@users.noreply.github.com>
2023-02-14 06:43:06 +0000

    feat(refresh-view): comment out unused views

--- lib/refresh-view.ts
+++ lib/refresh-view.ts
@@ -11,16 +11,16 @@ export const refreshView = async (view: string) => {
 };
 
 enum MATERIALIZED_VIEW {
-  tag_count_materialized = "tag_count_materialized",
+  // tag_count_materialized = "tag_count_materialized",
   user_reader_materialized = "user_reader_materialized",
   featured_comment_materialized = "featured_comment_materialized",
-  curation_tag_materialized = "curation_tag_materialized",
+  // curation_tag_materialized = "curation_tag_materialized",
   article_hottest_materialized = "article_hottest_materialized",
   most_active_author_materialized = "most_active_author_materialized",
   most_appreciated_author_materialized = "most_appreciated_author_materialized",
   most_trendy_author_materialized = "most_trendy_author_materialized",
   user_activity_materialized = "user_activity_materialized",
-  recently_read_tags_materialized = "recently_read_tags_materialized",
+  // recently_read_tags_materialized = "recently_read_tags_materialized",
   article_read_time_materialized = "article_read_time_materialized",
   recommended_articles_from_read_tags_materialized = "recommended_articles_from_read_tags_materialized",
 }
