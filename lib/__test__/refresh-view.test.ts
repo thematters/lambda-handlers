b038c35dd68a500a21dcbda60b04fce94701e6f3
dj <13580441+gary02@users.noreply.github.com>
2023-02-21 05:29:56 +0000

    test: fix tests

--- lib/__test__/refresh-view.test.ts
+++ lib/__test__/refresh-view.test.ts
@@ -2,8 +2,9 @@ import { refreshView } from "../refresh-view";
 
 test("unexpected view", async () => {
   await expect(refreshView("fake-view")).rejects.toThrow();
+  await expect(refreshView("tag_count_materialized")).rejects.toThrow();
 });
 
 test("expected view", async () => {
-  await refreshView("tag_count_materialized");
+  await refreshView("article_hottest_materialized");
 });
