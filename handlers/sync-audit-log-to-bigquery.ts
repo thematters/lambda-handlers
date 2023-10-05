f6798024e73fe86ceca37d85d421519268e8db26
gary <gary@e480>
2023-10-05 20:06:59 +0800

    feat(audit-log): add sync-audit-log-to-bigquery handler

---
+++ handlers/sync-audit-log-to-bigquery.ts
@@ -0,0 +1,96 @@
+import type { S3Event } from "aws-lambda";
+
+import { S3 } from "@aws-sdk/client-s3";
+import { BigQuery } from "@google-cloud/bigquery";
+
+import * as fs from "fs";
+import { createGunzip } from "zlib";
+import { createInterface } from "readline";
+
+const auditLogLabel = "audit-log";
+const projectId = process.env.MATTERS_BIGQUERY_PROJECT_ID;
+const datasetId = process.env.MATTERS_BIGQUERY_DATASET_ID || "";
+const tableId = process.env.MATTERS_BIGQUERY_TABLE_ID || "";
+const clientEmail = process.env.MATTERS_BIGQUERY_CLIENT_EMAIL;
+const privateKey = process.env.MATTERS_BIGQUERY_PRIVATE_KEY;
+const s3Bucket = process.env.MATTERS_S3_BUCKET || "";
+
+const s3 = new S3({});
+const bigquery = new BigQuery({
+  projectId,
+  credentials: {
+    client_email: clientEmail,
+    private_key: privateKey,
+  },
+});
+
+export const handler = async (event: S3Event) => {
+  console.log(event);
+  const fileKey = event.Records[0].s3.object.key;
+  if (!fileKey.includes("stdouterr.log")) {
+    console.log(`${fileKey} skipped`);
+    return;
+  }
+  const dst = "/tmp/" + event.Records[0].s3.object.key + ".json";
+  await processAndDumpLocal(fileKey, dst);
+  await uploadToBigQuery(dst);
+};
+
+// helpers
+
+const readFile = async (path: string) => {
+  const response = await s3.getObject({ Bucket: s3Bucket, Key: path });
+  const stream = response.Body as NodeJS.ReadableStream;
+
+  return createInterface({
+    input: stream.pipe(createGunzip()),
+    crlfDelay: Infinity,
+  });
+};
+
+const processAndDumpLocal = async (src: string, dst: string) => {
+  const lineReader = await readFile(src);
+  const writeStream = fs.createWriteStream(dst);
+
+  for await (const line of lineReader) {
+    const splits = line.split(" ");
+    if (splits.length < 5) {
+      continue;
+    }
+    const timestamp = splits[0] + " " + splits[1];
+    const requestId = splits[2];
+    const label = splits[3];
+    // const level = splits[4]
+    const message = splits.slice(5).join(" ");
+    if (label === auditLogLabel) {
+      const data = JSON.parse(message);
+      writeStream.write(
+        JSON.stringify({
+          actor_id: data.actorId,
+          action: data.action ? data.action.slice(0, 50) : undefined,
+          entity: data.entity ? data.entity.slice(0, 50) : undefined,
+          entity_id: data.entityId,
+          old_value: data.oldValue ? data.oldValue.slice(0, 255) : undefined,
+          new_value: data.newValue ? data.newValue.slice(0, 255) : undefined,
+          status: data.status ? data.status.slice(0, 10) : undefined,
+          request_id: requestId ? requestId.slice(0, 36) : undefined,
+          created_at: timestamp,
+        }) + "\n"
+      );
+    }
+  }
+};
+
+const uploadToBigQuery = async (src: string) => {
+  const [job] = await bigquery
+    .dataset(datasetId)
+    .table(tableId)
+    .load(src, { format: "json" });
+
+  console.log(`Job ${job.id} completed.`);
+
+  const errors = job?.status?.errors;
+  if (errors && errors.length > 0) {
+    throw errors;
+  }
+};
