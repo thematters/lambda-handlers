878d33bdcb1d9328d33ac907865ce589e3e6933c
xdj <13580441+gary02@users.noreply.github.com>
2023-11-15 16:37:08 +0800

    feat(ga4): improve import script

--- bin/sync-ga4-data.ts
+++ bin/sync-ga4-data.ts
@@ -2,11 +2,25 @@ import fs from "fs";
 import { fetchGA4Data, convertAndMerge, saveGA4Data } from "../lib/ga4.js";
 
 const main = async () => {
-  const startDate = "2021-10-15";
-  const endDate = "2023-10-19";
-  const data = await fetchGA4Data({ startDate, endDate });
-  fs.writeFileSync("./data.json", JSON.stringify(data));
-  const convertedData = await convertAndMerge(data);
+  const startDate = "2021-11-15";
+  const endDate = "2023-11-15";
+  const dataPath = `/tmp/lambda-handlers-sync-ga4-data(${startDate}-${endDate}).json`;
+  let data;
+  if (fs.existsSync(dataPath)) {
+    data = JSON.parse(fs.readFileSync(dataPath, "utf-8"));
+  } else {
+    data = await fetchGA4Data({ startDate, endDate });
+    fs.writeFileSync(dataPath, JSON.stringify(data));
+  }
+  const convertedPath = `/tmp/lambda-handlers-sync-ga4-data-converted(${startDate}-${endDate}).json`;
+  let convertedData;
+  if (fs.existsSync(convertedPath)) {
+    convertedData = JSON.parse(fs.readFileSync(convertedPath, "utf-8"));
+  } else {
+    convertedData = await fetchGA4Data({ startDate, endDate });
+    fs.writeFileSync(convertedPath, JSON.stringify(data));
+  }
+  convertedData = await convertAndMerge(data);
   await saveGA4Data(convertedData, { startDate, endDate });
 };
 
