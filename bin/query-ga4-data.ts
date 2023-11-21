30a1929b4e88fc1f5e11df09b0c15f5249d48db3
gary <gary@e480>
2023-11-21 12:29:56 +0800

    fix(ga4): add new events as ga4 has a data processing delay of 24-48 hours

--- bin/query-ga4-data.ts
+++ bin/query-ga4-data.ts
@@ -6,8 +6,8 @@ const clientEmail = process.env.MATTERS_GA4_CLIENT_EMAIL;
 const privateKey = process.env.MATTERS_GA4_PRIVATE_KEY || "";
 
 const main = async () => {
-  const startDate = "2023-11-20";
-  const endDate = "2023-11-20";
+  const startDate = "2023-11-21";
+  const endDate = "2023-11-21";
   const client = new BetaAnalyticsDataClient({
     projectId,
     credentials: {
--- handlers/sync-ga4-data.ts
+++ handlers/sync-ga4-data.ts
@@ -14,13 +14,17 @@ import {
 // AWS EventBridge can configure the input event sent to Lambda,
 // see https://docs.aws.amazon.com/eventbridge/latest/userguide/eb-transform-target-input.html for info.
 type Event = {
-  type: "today" | "yesterday";
+  type: "today" | "yesterday" | "2 days ago" | "3 days ago";
 };
 
-const getDate = (type: "today" | "yesterday") => {
+const getDate = (type: Event["type"]) => {
   const date = new Date();
   if (type === "yesterday") {
     date.setDate(date.getDate() - 1);
+  } else if (type === "2 days ago") {
+    date.setDate(date.getDate() - 2);
+  } else if (type === "3 days ago") {
+    date.setDate(date.getDate() - 3);
   }
   return getLocalDateString(date);
 };
